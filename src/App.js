cb6fb38c352739d44b0d88590529d9a68915d084
Taylor Sampson <thsampsn@gmail.com>
2020-09-11 11:23:29 -0400

    new repo

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="mainDiv">
+      </div>
+      <p>Welcome!</p>
+    </div>
+  );
+}
+
+export default App;
